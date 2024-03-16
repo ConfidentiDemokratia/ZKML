4e91485f33e67d71c56cfa1f15ff511b78da23c5
Galim Usaev <55399978+galimus@users.noreply.github.com>
2024-03-16 11:53:50 +0000

    Update setup.sh

--- EZKL_USE_server/setup.sh
+++ EZKL_USE_server/setup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# Create a virtual environment named 'env' if it doesn't already exist
+if [ ! -d "env" ]; then
+    python3 -m venv env
+fi
+
+# Activate the virtual environment
+source env/bin/activate
+
 # Install Python packages
+pip install --upgrade pip
 pip install -r requirements.txt
 
 # Install and use a specific version of solc via solc-select
@@ -8,4 +17,7 @@ solc-select install 0.8.20
 solc-select use 0.8.20
 
 # Run the Python script
-python3 ezkl_process.py network.onnx input.json
+python3 ezkl_process.py model.onnx input.json
+
+# Deactivate the virtual environment
+deactivate
